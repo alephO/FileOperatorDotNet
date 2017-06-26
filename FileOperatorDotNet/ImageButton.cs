683c9c1f9acd71362ea6e1e55218368b06bb78c8
Jialong Chen <jlch@brandeis.edu>
2017-06-26 18:12:10 -0400

    Monday June 26

---
+++ FileOperatorDotNet/ImageButton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FileOperatorDotNet
+{
+    class ImageButton: Button
+    {
+        public ImageSource BNorImage
+        {
+            get { return (ImageSource)GetValue(BNorImageProperty); }
+            set { SetValue(BNorImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty BNorImageProperty =
+            DependencyProperty.Register("BNorImage", typeof(ImageSource), typeof(ImageTabItem));
+
+        public ImageSource BHorImage
+        {
+            get { return (ImageSource)GetValue(BHorImageProperty); }
+            set { SetValue(BHorImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty BHorImageProperty =
+            DependencyProperty.Register("BHorImage", typeof(ImageSource), typeof(ImageTabItem));
+
+
+        public ImageSource BDownImage
+        {
+            get { return (ImageSource)GetValue(BDownImageProperty); }
+            set { SetValue(BDownImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty BDownImageProperty =
+            DependencyProperty.Register("BDownImage", typeof(ImageSource), typeof(ImageTabItem));
+
+    }
+}
